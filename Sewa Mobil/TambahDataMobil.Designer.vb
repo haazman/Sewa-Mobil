6781e8807bf1d8e172e20a95fdf35c0e219fcf78
ukino <arfianoj@gmail.com>
2022-12-29 03:32:12 +0700

    mobil

---
+++ Sewa Mobil/TambahDataMobil.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TambahDataMobil
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.fotoUser = New System.Windows.Forms.PictureBox()
+        Me.tbEmail = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tbUsername = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.addCarBtn = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.fotoUser, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.fotoUser)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.Location = New System.Drawing.Point(177, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(117, 158)
+        Me.GroupBox1.TabIndex = 21
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Add Your Picture Here"
+        '
+        'fotoUser
+        '
+        Me.fotoUser.Location = New System.Drawing.Point(6, 13)
+        Me.fotoUser.Name = "fotoUser"
+        Me.fotoUser.Size = New System.Drawing.Size(105, 140)
+        Me.fotoUser.TabIndex = 0
+        Me.fotoUser.TabStop = False
+        '
+        'tbEmail
+        '
+        Me.tbEmail.Location = New System.Drawing.Point(29, 269)
+        Me.tbEmail.Name = "tbEmail"
+        Me.tbEmail.Size = New System.Drawing.Size(188, 23)
+        Me.tbEmail.TabIndex = 20
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 251)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 15)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "Email"
+        '
+        'tbUsername
+        '
+        Me.tbUsername.Location = New System.Drawing.Point(29, 210)
+        Me.tbUsername.Name = "tbUsername"
+        Me.tbUsername.Size = New System.Drawing.Size(188, 23)
+        Me.tbUsername.TabIndex = 18
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 192)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 15)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Merek Mobil"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(29, 333)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(188, 23)
+        Me.TextBox1.TabIndex = 23
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(29, 315)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 15)
+        Me.Label3.TabIndex = 22
+        Me.Label3.Text = "Email"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(29, 401)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(188, 23)
+        Me.TextBox2.TabIndex = 25
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 383)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(36, 15)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "Email"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(254, 210)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(188, 23)
+        Me.TextBox3.TabIndex = 27
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(254, 192)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(36, 15)
+        Me.Label5.TabIndex = 26
+        Me.Label5.Text = "Email"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(254, 269)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(188, 23)
+        Me.TextBox4.TabIndex = 29
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(254, 251)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(36, 15)
+        Me.Label6.TabIndex = 28
+        Me.Label6.Text = "Email"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(254, 333)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(188, 23)
+        Me.TextBox5.TabIndex = 31
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(254, 315)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(36, 15)
+        Me.Label7.TabIndex = 30
+        Me.Label7.Text = "Email"
+        '
+        'addCarBtn
+        '
+        Me.addCarBtn.Location = New System.Drawing.Point(300, 395)
+        Me.addCarBtn.Name = "addCarBtn"
+        Me.addCarBtn.Size = New System.Drawing.Size(106, 32)
+        Me.addCarBtn.TabIndex = 32
+        Me.addCarBtn.Text = "Add Car"
+        Me.addCarBtn.UseVisualStyleBackColor = True
+        '
+        'TambahDataMobil
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(472, 480)
+        Me.Controls.Add(Me.addCarBtn)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.tbEmail)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.tbUsername)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "TambahDataMobil"
+        Me.Text = "TambahDataMobil"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.fotoUser, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents fotoUser As PictureBox
+    Friend WithEvents tbEmail As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents tbUsername As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents addCarBtn As Button
+End Class
---
+++ Sewa Mobil/TambahDataMobil.vb
@@ -0,0 +1,3 @@
+﻿Public Class TambahDataMobil
+
+End Class
