905ab03f65237eb7f60c47d385656952c4a49e4b
ukino <arfianoj@gmail.com>
2022-12-28 20:42:43 +0700

    login, logout, sign up

---
+++ Sewa Mobil/mainMenu.vb
@@ -0,0 +1,8 @@
+﻿Public Class mainMenu
+    Private Sub logoutBtn_Click(sender As Object, e As EventArgs) Handles logoutBtn.Click
+        Me.Close()
+        SignIn.Show()
+
+
+    End Sub
+End Class
