f99ea1723c2143b04a5645cff61b6871aeb4da6b
ukino <arfianoj@gmail.com>
2022-12-29 01:28:30 +0700

    form mobil

---
+++ Sewa Mobil/mobil.vb
@@ -0,0 +1,19 @@
+﻿Public Class mobil
+    Public Shared DataMobil = New DataMobil()
+    Private Sub mobil_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ReloadDataTableDatabase()
+        DataGridMobil.DataSource = DataMobil.GetDataMobilDatabase()
+    End Sub
+
+    Private Sub mobil_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        ReloadDataTableDatabase()
+    End Sub
+
+    Private Sub addCarBtn_Click(sender As Object, e As EventArgs) Handles addCarBtn.Click
+        Me.Hide()
+        TambahDataMobil.Show()
+    End Sub
+End Class
